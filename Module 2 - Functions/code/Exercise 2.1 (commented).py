b89b590773ad05a2dd720b0425344880899c910a
Henry <henrykdoyle@icloud.com>
2025-02-21 10:37:04 +1000

    Finish comment on exercise 2.1. Create python script for Exercise 2.1 and right_justify. Copy right_justify into new folder CSC1401/_functions

---
+++ Module 2 - Functions/code/Exercise 2.1 (commented).py
@@ -0,0 +1,45 @@
+def right_justify(s):
+    # docstring here
+    """
+    Summary:
+        right_justify takes an input string and adds space characters to the start.
+        The result is 70 characters long, with the final character of the input string in the 70th column.
+
+    Parameters:
+        s (str) - input string
+        stringlength (int) - length of input string
+        outputlength (int) - lenth of output string
+        output(str) - output string
+    """
+    output = "" # output is empty at this stage. this is because the characters need to be addes to output first. the result of output will be redefined later in the function
+    stringlength = len(s)
+    outputlength = 70-stringlength
+
+    for i in range(outputlength):
+        output += " "
+        # '+=' is a modifier that performs string concatenation. 
+        # it iterates over the string and appends the value: + the new value to the previous string, = the result of the addition to the variable
+        #
+        # the for loop is what repeats the process. Loop works like this:
+        #    1: Take the range of the output length (in this case, 70 minus the length of the input string) and use this value as the range for the loop
+        #    2: Concatenate a whitespace to the output 
+        #    3: let the output equal the new concatenated value
+        #    4: Loop finished. Haed back to the start having completed one loop of the total umber (range) of loops.
+        #    5: Repeat until range is exhaused 
+    
+    output += s
+    # '+=' here takes the concatenated whitespace string and appends the input string to the whitespace string
+
+    print(output) # this is the final stap of the function
+                  # output has been redefined as (number of space characters defined by outputlength) + s (the input string from right_justify)
+                  # printing this concatenated output reveals text that is "justified" to column 70
+
+
+
+##### TESTING #####
+
+right_justify('test')
+right_justify('monty')
+right_justify("if somebody told me i'd die at 40, i wouldn't have spent my time in bed")
+# the above string is 72 characters. this is above the 70 character space appending, which means that no spaces are appended.
+# this result just outside  of the size, so it will spill over. it will still be "justified" but just not appear that way.
